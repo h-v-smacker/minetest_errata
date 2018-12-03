8a828699b8681c5cc91393414e67df1146f93111
h-v-smacker <hans-von-smacker+github@gmail.com>
2018-12-03 19:42:35 +0300

    better abm reporting

--- chat_commands.lua
+++ chat_commands.lua
@@ -2,7 +2,10 @@ minetest_errata.stats = {
 	n = "pending recount...",
 	c = "pending recount...",
 	t = "pending recount...",
-	e = "pending recount..."
+	e = "pending recount...",
+	a = "pending recount...",
+	g = "pending recount...",
+	abms = {},
 }
 
 minetest.after(30, 
@@ -27,7 +30,21 @@ minetest.after(30,
 		for _,i in pairs(minetest.registered_entities) do
 			n.e = n.e + 1
 		end
-
+              
+		n.a = 0
+		n.abms = {}
+		for _,i in pairs(minetest.registered_abms) do
+			n.a = n.a + 1
+			local l = i.label or "no label"
+			table.insert(n.abms, {l, i.interval, i.chance, i.nodenames})
+		end
+-- 		n.a = #n.abms
+              
+		n.g = 0
+		for _,i in pairs(minetest.registered_globalsteps) do
+			n.g = n.g + 1
+		end
+              
 		minetest_errata.stats = n
 	end
 )
@@ -42,7 +59,24 @@ minetest.register_chatcommand("showmewhatyougot", {
 						"Registered nodes: " .. tostring(minetest_errata.stats.n) .. "\n" ..
 						"Registered craftitems: " .. tostring(minetest_errata.stats.c) .. "\n" ..
 						"Registered tools: " .. tostring(minetest_errata.stats.t) .. "\n" ..
-						"Registered entities: " .. tostring(minetest_errata.stats.e) .. "\n")
+						"Registered entities: " .. tostring(minetest_errata.stats.e) .. "\n" ..
+						"Registered ABMs: " .. tostring(minetest_errata.stats.a) .. "\n" ..
+						"Registered globalsteps: " .. tostring(minetest_errata.stats.g) .. "\n"
+                                     )
+		return true
+	end,
+})
+
+minetest.register_chatcommand("showmewhatyougot_abms", {
+	params = "",
+	description = "Detailed stats on abms",
+	privs = { shout = true },
+	func = function( name )
+		
+		for _,l in ipairs(minetest_errata.stats.abms) do
+			minetest.chat_send_player(name, l[1] .. " - " .. l[2] .. " (" .. l[3] .. ") - " .. table.concat(l[4], ", "))
+		end
+                                                       
 		return true
 	end,
 })
